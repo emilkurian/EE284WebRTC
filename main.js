efd6df3668628b7e046721788bc46be624f7db35
emilkurian <emilkurian@gmail.com>
2020-12-06 12:11:54 -0800

    Update main.js

--- main.js
+++ main.js
@@ -11,56 +11,28 @@ let room;
 let pc;
 let localStream;
 
-// Define action buttons.
-const callButton = document.getElementById('callButton');
-const hangupButton = document.getElementById('hangupButton');
-
-// Set up initial action buttons status: disable call and hangup.
-callButton.disabled = false;
-hangupButton.disabled = true;
-
-// functions
-callButton.addEventListener('click', callAction);
-hangupButton.addEventListener('click', hangupAction);
-
-function trace(message) {
-  console.log(message)
-};
-function traceError(error) {
+function onSuccess() {};
+function onError(error) {
   console.error(error);
 };
-        
-function callAction() {
-  callButton.disabled = true;
-  hangupButton.disabled = false;
-  trace('Starting call.');
-  startWebRTC(true);
-  trace('Message Sent')
-}
-  
-function hangupAction() {
-  pc.close();
-  pc = null;
-  hangupButton.disabled = true;
-  callButton.disabled = false;
-  trace('Ending call.');
-}
-        
+
 drone.on('open', error => {
   if (error) {
-    return traceError(error);
+    return console.error(error);
   }
   room = drone.subscribe(roomName);
   room.on('open', error => {
     if (error) {
-      traceError(error);
+      onError(error);
     }
   });
-  trace(roomName);
   // We're connected to the room and received an array of 'members'
   // connected to the room (including us). Signaling server is ready.
   room.on('members', members => {
-    trace('MEMBERS:' + members.length);
+    console.log('MEMBERS', members);
+    // If we are the second user to connect to the room we will be creating the offer
+    const isOfferer = members.length === 2;
+    startWebRTC(isOfferer);
   });
 });
 
@@ -74,22 +46,19 @@ function sendMessage(message) {
 
 function startWebRTC(isOfferer) {
   pc = new RTCPeerConnection(configuration);
-  
-  
+
   // 'onicecandidate' notifies us whenever an ICE agent needs to deliver a
   // message to the other peer through the signaling server
   pc.onicecandidate = event => {
     if (event.candidate) {
       sendMessage({'candidate': event.candidate});
-      trace('Ice candidate' + event.candidate);
     }
   };
 
   // If user is offerer let the 'negotiationneeded' event create the offer
   if (isOfferer) {
     pc.onnegotiationneeded = () => {
-      pc.createOffer().then(localDescCreated).catch(traceError);
-      trace('Offer Created');
+      pc.createOffer().then(localDescCreated).catch(onError);
     }
   }
 
@@ -99,7 +68,6 @@ function startWebRTC(isOfferer) {
     if (!remoteVideo.srcObject || remoteVideo.srcObject.id !== stream.id) {
       remoteVideo.srcObject = stream;
     }
-    trace('remoteStream');
   };
 
   navigator.mediaDevices.getUserMedia({
@@ -108,10 +76,10 @@ function startWebRTC(isOfferer) {
   }).then(stream => {
     // Display your local video in #localVideo element
     localVideo.srcObject = stream;
-    // Add your stream to be sent to the connecting peer
+    // Add your stream to be sent to the conneting peer
     stream.getTracks().forEach(track => pc.addTrack(track, stream));
-  }, traceError);
-  
+  }, onError);
+
   // Listen to signaling data from Scaledrone
   room.on('data', (message, client) => {
     // Message was sent by us
@@ -124,13 +92,13 @@ function startWebRTC(isOfferer) {
       pc.setRemoteDescription(new RTCSessionDescription(message.sdp), () => {
         // When receiving an offer lets answer it
         if (pc.remoteDescription.type === 'offer') {
-          pc.createAnswer().then(localDescCreated).catch(traceError);
+          pc.createAnswer().then(localDescCreated).catch(onError);
         }
-      }, traceError);
+      }, onError);
     } else if (message.candidate) {
       // Add the new ICE candidate to our connections remote description
       pc.addIceCandidate(
-        new RTCIceCandidate(message.candidate), trace, traceError
+        new RTCIceCandidate(message.candidate), onSuccess, onError
       );
     }
   });
@@ -140,6 +108,6 @@ function localDescCreated(desc) {
   pc.setLocalDescription(
     desc,
     () => sendMessage({'sdp': pc.localDescription}),
-    traceError
+    onError
   );
 }
